2cf4aceade39da878cb372db9adcf0b7ad6b3e72
Urmila4718 <79640277+Urmila4718@users.noreply.github.com>
2024-02-14 23:04:39 +0530

    Create plus_minus.py

---
+++ plus_minus.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    p=0
+    n1=0
+    z=0
+    for i in range(len(arr)):
+        if arr[i]>0:
+            p+=1
+        elif arr[i]<0:
+            n1+=1
+        else:
+            z+=1
+        
+    print("{:.6f}".format(p/n), "\n", "{:.6f}".format(n1/n), "\n", "{:.6f}".format(z/n))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
+
+
+# input 
+# 6
+# -4 3 -9 0 4 1
+
+# Ouput 
+# 0.500000
+# 0.333333
+# 0.166667
